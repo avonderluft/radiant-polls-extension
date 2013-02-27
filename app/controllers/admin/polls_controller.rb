be93998f7c1baee5e700b971cb8d09a7af99517c
Andrew vonderLuft <avonderluft@avlux.net>
2013-02-27 14:05:27 -0800

    use pagination built-in to Radiant

--- app/controllers/admin/polls_controller.rb
+++ app/controllers/admin/polls_controller.rb
@@ -1,5 +1,11 @@
 class Admin::PollsController < Admin::ResourceController
+
   model_class Poll
+  paginate_models
+  # only_allow_access_to :index, :show, :new, :create, :edit, :update, :remove, :destroy,
+  #   :when => [:designer, :admin],
+  #   :denied_url => { :controller => 'admin/pages', :action => 'index' },
+  #   :denied_message => 'You must have designer privileges to perform this action.'
 
   def clear_responses
     if @poll = Poll.find(params[:id])
@@ -13,7 +19,7 @@ class Admin::PollsController < Admin::ResourceController
 
     def load_models
       # Order polls by descending date, then alphabetically by title
-      self.models = model_class.all(:order => Poll.send(:sanitize_sql_array, [ "COALESCE(start_date, ?) DESC, title", Date.new(1900, 1, 1) ]))
+      self.models = model_class.all(:order => Poll.send(:sanitize_sql_array, [ "COALESCE(start_date, ?) DESC, title", Date.new(1900, 1, 1) ])).paginate(pagination_parameters)
     end
 
 end
